176e5620b58750a3deffd100b84a8a457c5976b4
netizen-om <borisagarom@gmail.com>
2025-05-24 23:50:19 +0530

    added sort of credentials based signin

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,46 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaClient } from '@prisma/client'
 
-// const handler : unknown= NextAuth({
-    
-// })
+// const prisma = new PrismaClient()
 
-const handler = () => {
-    console.log("hy");
-}
+const handler = NextAuth({
+    providers : [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+            username: { label: 'email', type: 'text', placeholder: '' },
+            password: { label: 'password', type: 'password', placeholder: '' },
+            }, 
+
+            async authorize(credentials) { 
+                
+                const email = credentials?.username; 
+                const password = credentials?.password; 
+
+                // const user = await prisma.user.findone({
+                //     email,
+                //     password
+                // })
+
+                
+                const user = 1
+
+                if(!user) {
+                    return null;
+                }
+
+                return {
+                    // commented to remove errors
+                    // id: user.id,
+                    // email : user.email,
+                    id : "test123"
+                };
+            },
+        })
+    ],
+    secret : process.env.NEXTAUTH_URL
+})
+
+export const GET = handler
+export const POST = handler
