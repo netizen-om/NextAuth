70d9ec65452d3b6ae577792560ece2ca5cc1af46
netizen-om <borisagarom@gmail.com>
2025-05-25 12:36:12 +0530

    completed id and password based Auth using NextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,13 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaClient } from '@prisma/client'
+import { Session } from "inspector/promises";
+import { NEXT_AUTH } from "@/app/lib/auth";
 
 // const prisma = new PrismaClient()
 
-const handler = NextAuth({
-    providers : [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-            username: { label: 'email', type: 'text', placeholder: '' },
-            password: { label: 'password', type: 'password', placeholder: '' },
-            }, 
+const handler = NextAuth(NEXT_AUTH)  
 
-            async authorize(credentials) { 
-                
-                const email = credentials?.username; 
-                const password = credentials?.password; 
-
-                // const user = await prisma.user.findone({
-                //     email,
-                //     password
-                // })
-
-                
-                const user = 1
-
-                if(!user) {
-                    return null;
-                }
-
-                return {
-                    // commented to remove errors
-                    // id: user.id,
-                    // email : user.email,
-                    id : "test123",
-                    name : "om123",
-                    email: "om@om.com"
-                };
-            },
-        })
-    ],
-    secret : process.env.NEXTAUTH_URL
-})
 
 export const GET = handler
 export const POST = handler
---
+++ app/lib/auth.ts
@@ -0,0 +1,59 @@
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const NEXT_AUTH = {
+    providers : [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+            username: { label: 'email', type: 'text', placeholder: '' },
+            password: { label: 'password', type: 'password', placeholder: '' },
+            }, 
+
+            async authorize(credentials) { 
+                
+                const email = credentials?.username; 
+                const password = credentials?.password; 
+
+                // const user = await prisma.user.findone({
+                //     email,
+                //     password
+                // })
+
+                
+                const user = 1
+
+                if(!user) {
+                    return null;
+                }
+
+                return {
+                    // commented to remove errors
+                    // id: user.id,
+                    // email : user.email,
+                    id : "12413123",
+                    name : "om123",
+                    email: "om@om.com"
+                };
+            },
+        })
+    ],
+    secret : process.env.NEXTAUTH_URL,
+    callbacks : {
+        jwt : ({ token, user } : any) => {
+            // console.log(token);
+            token.userId = token.sub;
+            return token;
+            
+        },
+        session : ({session, token, user } :any) => {
+            if(session && session.user) {
+                session.user.id = token.sub // or can be token.userId due to jwt callback
+            }
+            console.log(session);
+            return session
+            
+        }
+    },
+    
+        
+}
